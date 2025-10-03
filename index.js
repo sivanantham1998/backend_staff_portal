8761cb16fc35fc481e359639c5b0e3cbed4df069
Segolsysit <segolsys@gmail.com>
2025-10-03 10:26:59 +0530

    demo

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(
   cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    // credentials: true,
     origin: "http://localhost:5173",
   })
 );
